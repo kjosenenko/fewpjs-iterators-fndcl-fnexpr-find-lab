9c436643d95b7eafbc2f0adb40ede649ab29d8c1
KJ Osenenko <karljosenenko@gmail.com>
2021-02-23 12:28:14 -0700

    commit tree 25f71b6c03e37c8b13979835adcc266953e3353c

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+const record = [
+    {year: "2018", result: "L"},
+    {year: "2017", result: "W"},
+    {year: "2016", result: "N/A"}
+  ]
 
+function superbowlWin(record) {
+    const season = record.find( season => season.result === "W")
+    return !!season ? season.year : undefined
+}
